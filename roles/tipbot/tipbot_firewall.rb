404ea6d79e1b777af75fb7420872ee55e6a16a6e
vindimy <vindimy@gmail.com>
2013-09-13 23:51:10 -0700

    Updating dv's ssh ke. Consolidating roles. Updating bootstrap file.

--- roles/tipbot/tipbot_firewall.rb
+++
@@ -1,8 +0,0 @@
-name "tipbot_firewall"
-description "Role to deploy firewall rules for tip bot"
-run_list(
-  "recipe[firewall]",
-  "recipe[app_tipbot::firewall]"
-)
-override_attributes(
-)
--- roles/tipbot/tipbot_python.rb
+++
@@ -1,9 +0,0 @@
-name "tipbot_python"
-description "Role to deploy python language for tip bot"
-run_list(
-  "recipe[python]",
-  "recipe[mysql::client]",
-  "recipe[app_tipbot::python]"
-)
-override_attributes(
-)
