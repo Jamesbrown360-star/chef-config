d07f038d1894654f7aad5910d48a0c23d7cc40b0
vindimy <vindimy@gmail.com>
2013-09-25 18:46:41 -0400

    Improving

--- scripts/knife-bootstrap.sh
+++ scripts/knife-bootstrap.sh
@@ -3,21 +3,21 @@
 fqdn=$1
 chef_environment=$2
 run_list=$3
+ssh_opts=$4
 
 if [ -z "$run_list" ] ; then
-	echo "Usage: $0 FQDN ENVIRONMENT RUNLIST"
+	echo "Usage: $0 FQDN ENVIRONMENT RUNLIST [SSH-OPTS]"
 	exit 1
 fi
 
 # Set FQDN on the host and remove /etc/chef directory if present
-#ssh root@$fqdn "hostname $fqdn; echo $fqdn > /etc/hostname; rm -rf /etc/chef" || exit 2
+ssh root@$fqdn $ssh_opts "hostname $fqdn; echo $fqdn > /etc/hostname; rm -rf /etc/chef" || exit 1
 
 # Delete previous node and client record(s)
-#knife node delete $fqdn
-#knife client delete $fqdn
+knife node delete $fqdn
+knife client delete $fqdn
 
 # Bootstrap
-echo knife bootstrap $fqdn -N $fqdn -E $chef_environment -x root -r "$run_list" 
-knife bootstrap $fqdn -N $fqdn -E $chef_environment -x root -r "$run_list" || exit 3
+knife bootstrap $fqdn -N $fqdn -E $chef_environment -x root $ssh_opts -r "$run_list" || exit 1
 
 exit 0
