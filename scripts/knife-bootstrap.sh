c25363f6a43ec846f5878171fd8761213b2c65bb
root <root@katara.thedimi.net>
2016-10-25 00:40:17 +0000

    removing extra commands

--- scripts/knife-bootstrap.sh
+++ scripts/knife-bootstrap.sh
@@ -11,11 +11,11 @@ if [ -z "$run_list" ] ; then
 fi
 
 # Set FQDN on the host and remove /etc/chef directory if present
-ssh root@$fqdn $ssh_opts "hostname $fqdn; echo $fqdn > /etc/hostname; rm -rf /etc/chef" || exit 1
+#ssh root@$fqdn $ssh_opts "hostname $fqdn; echo $fqdn > /etc/hostname; rm -rf /etc/chef" || exit 1
 
 # Delete previous node and client record(s)
-knife node delete $fqdn
-knife client delete $fqdn
+#knife node delete $fqdn
+#knife client delete $fqdn
 
 # Bootstrap
 knife bootstrap $fqdn -N $fqdn -E $chef_environment -x root $ssh_opts -r "$run_list" || exit 1
